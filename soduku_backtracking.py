dcf8e086e3d57e0377735d3b0a13f24e964ee8ed
1ai7h <81769715+1ai7h@users.noreply.github.com>
2022-01-30 22:34:23 +0200

    Update soduku_backtracking.py

--- soduku_backtracking.py
+++ soduku_backtracking.py
@@ -10,16 +10,21 @@ grid = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
-print(np.matrix(grid))
+print(np.matrix(grid)) #Creates the Matrix using Numpy to make things easier
 
-def check_valid(y,x,n):
+def check_valid(y,x,n): #Check if the number n can be fit in the graph with x,y position
     global grid
     for i in range(0,9):
-        if grid[y][i] == n:
+        if grid[y][i] == n: #if the 9 columns have the number present then we cannot add it in
             return False
-    for i in range(0,9):
+    for i in range(0,9): #if the 9 rows have the number in them we cannot add them in
         if grid[i][x] == n:
             return False
+        '''
+        This next piece of code was used in order to handle the boxes, one of the rules
+        of soduku is that you cannot have the same number in the row, column, OR box. from our visualization
+        we find that we have a set of 9 boxes grouped together which cannot have identical numbers.
+        '''
     x0 = (x//3) * 3
     y0 = (y//3) * 3
     for i in range(0,3):
@@ -30,15 +35,15 @@ def check_valid(y,x,n):
   
   def solve():
     global grid
-    for y in range(9) :
-        for x in range(9) :
-            if grid[y][x] == 0 :
-                for n in range(1,10) :
-                    if check_valid(y,x,n):
-                        grid[y][x] = n
-                        solve()
-                        grid[y][x] = 0
-                return
-    print(np.matrix(grid))
+    for y in range(9) : #for all y values (columns)
+        for x in range(9) : #for all x values (rows)
+            if grid[y][x] == 0 : #if there is an empty space
+                for n in range(1,10) : #try out all numbers using brute force by passing the number through our function check_valud
+                    if check_valid(y,x,n): #if found
+                        grid[y][x] = n #set the number 
+                        solve() #recursively do this all over again 
+                        grid[y][x] = 0 #if the number is not valid, set it to 0 
+                return 
+    print(np.matrix(grid)) #show grid
 
 solve()
